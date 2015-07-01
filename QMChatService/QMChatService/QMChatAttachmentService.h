b0b9ef7dd3197b0e4aa6e456aac59d2f0f736ec6
Gleb Ustimenko <gleb.ustimenko@injoit.com>
2015-07-01 17:28:11 +0300

    QBIOSSDK-258

---
+++ QMChatService/QMChatService/QMChatAttachmentService.h
@@ -0,0 +1,30 @@
+//
+//  QMChatAttachmentService.h
+//  QMChatService
+//
+//  Created by Injoit on 7/1/15.
+//
+//
+
+#import <Foundation/Foundation.h>
+#import "QMChatTypes.h"
+
+@class QMChatService;
+@class QMChatAttachmentService;
+
+@protocol QMChatAttachmentServiceDelegate <NSObject>
+
+- (void)chatAttachmentService:(QMChatAttachmentService *)chatAttachmentService didChangeAttachmentStatus:(QMMessageAttachmentStatus)status forMessage:(QBChatMessage *)message;
+
+@end
+
+@interface QMChatAttachmentService : NSObject
+
+@property (nonatomic, weak) id<QMChatAttachmentServiceDelegate> delegate;
+
+- (void)sendMessage:(QBChatMessage *)message toDialog:(QBChatDialog *)dialog withChatService:(QMChatService *)chatService withAttachedImage:(UIImage *)image completion:(void(^)(NSError *error))completion;
+
+- (void)getImageForChatAttachment:(QBChatAttachment *)attachment completion:(void (^)(NSError *error, UIImage *image))completion;
+
+
+@end
---
+++ QMChatService/QMChatService/QMChatAttachmentService.m
@@ -0,0 +1,124 @@
+//
+//  QMChatAttachmentService.m
+//  QMChatService
+//
+//  Created by Injoit on 7/1/15.
+//
+//
+
+#import "QMChatAttachmentService.h"
+#import "QMChatService.h"
+#import "QBChatMessage+QMCustomParameters.h"
+#import "QMChatService+AttachmentService.h"
+
+static NSString* attachmentCacheDir() {
+    
+    static NSString *attachmentCacheDirString;
+    
+    if (!attachmentCacheDirString) {
+        
+        NSString *cacheDir = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES).firstObject;
+        attachmentCacheDirString = [cacheDir stringByAppendingPathComponent:@"Attachment"];
+        
+        static dispatch_once_t onceToken;
+        
+        dispatch_once(&onceToken, ^{
+            if (![[NSFileManager defaultManager] fileExistsAtPath:attachmentCacheDirString]) {
+                [[NSFileManager defaultManager] createDirectoryAtPath:attachmentCacheDirString withIntermediateDirectories:NO attributes:nil error:nil];
+            }
+        });
+    }
+
+    return attachmentCacheDirString;
+}
+
+static NSString* attachmentPath(QBChatAttachment *attachment) {
+    
+    return [attachmentCacheDir() stringByAppendingPathComponent:[NSString stringWithFormat:@"attachment-%@", attachment.ID]];
+}
+
+@implementation QMChatAttachmentService
+
+
+- (void)sendMessage:(QBChatMessage *)message toDialog:(QBChatDialog *)dialog withChatService:(QMChatService *)chatService withAttachedImage:(UIImage *)image completion:(void (^)(NSError *))completion {
+    
+    [chatService.messagesMemoryStorage addMessage:message forDialogID:dialog.ID];
+    
+    [self changeMessageAttachmentStatus:QMMessageAttachmentStatusLoading forMessage:message];
+    
+    NSData *imageData = UIImagePNGRepresentation(image);
+    
+    [QBRequest TUploadFile:imageData fileName:@"attachment" contentType:@"image/png" isPublic:YES successBlock:^(QBResponse *response, QBCBlob *blob) {
+       
+        QBChatAttachment *attachment = [QBChatAttachment new];
+        attachment.type = @"image";
+        attachment.ID = [@(blob.ID) stringValue];
+        attachment.url = [blob publicUrl];
+        
+        message.attachments = @[attachment];
+        
+        [self saveImageData:imageData chatAttachment:attachment];
+        
+        [self changeMessageAttachmentStatus:QMMessageAttachmentStatusLoaded forMessage:message];
+        
+        [chatService sendMessage:message type:QMMessageTypeText toDialog:dialog save:YES saveToStorage:NO completion:completion];
+        
+    } statusBlock:^(QBRequest *request, QBRequestStatus *status) {
+        
+    } errorBlock:^(QBResponse *response) {
+        
+        [self changeMessageAttachmentStatus:QMMessageAttachmentStatusNotLoaded forMessage:message];
+        
+        if (completion) completion(response.error.error);
+    }];
+}
+
+- (void)getImageForChatAttachment:(QBChatAttachment *)attachment completion:(void (^)(NSError *error, UIImage *image))completion {
+    
+    NSString *path = attachmentPath(attachment);
+    
+    if ([[NSFileManager defaultManager] fileExistsAtPath:path]) {
+        
+        UIImage *image = [UIImage imageWithContentsOfFile:path];
+        
+        if (completion) completion(nil, image);
+        
+        return;
+    }
+    
+    [QBRequest downloadFileWithID:attachment.ID.integerValue successBlock:^(QBResponse *response, NSData *fileData) {
+        
+        UIImage *image = [UIImage imageWithData:fileData];
+        
+        [self saveImageData:fileData chatAttachment:attachment];
+        
+        if (completion) completion(nil, image);
+        
+    } statusBlock:^(QBRequest *request, QBRequestStatus *status) {
+        
+    } errorBlock:^(QBResponse *response) {
+        
+       if (completion) completion(response.error.error, nil);
+        
+    }];
+}
+
+- (void)saveImageData:(NSData *)imageData chatAttachment:(QBChatAttachment *)attachment {
+    
+    NSString *path = attachmentPath(attachment);
+    
+    [imageData writeToFile:path atomically:YES];
+}
+
+- (void)changeMessageAttachmentStatus:(QMMessageAttachmentStatus)status forMessage:(QBChatMessage *)message {
+    
+    message.attachmentStatus = status;
+    
+    if ([self.delegate respondsToSelector:@selector(chatAttachmentService:didChangeAttachmentStatus:forMessage:)]) {
+        [self.delegate chatAttachmentService:self didChangeAttachmentStatus:status forMessage:message];
+    }
+}
+
+
+
+@end
