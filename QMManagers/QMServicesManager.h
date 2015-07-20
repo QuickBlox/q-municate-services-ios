208fbb23e4ce46a73209a6e59ac7cab68942215e
Gleb Ustimenko <gleb.ustimenko@injoit.com>
2015-07-20 10:46:53 +0300

    Added QMServicesManager

---
+++ QMManagers/QMServicesManager.h
@@ -0,0 +1,22 @@
+//
+//  QBServiceManager.h
+//  sample-chat
+//
+//  Created by Andrey Moskvin on 5/19/15.
+//  Copyright (c) 2015 Igor Khomenko. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "QMServices.h"
+
+@interface QMServicesManager : NSObject <QMServiceManagerProtocol, QMChatServiceCacheDataSource, QMContactListServiceCacheDataSource, QMChatServiceDelegate, QMChatConnectionDelegate>
+
++ (instancetype)instance;
+
+- (void)logInWithUser:(QBUUser *)user completion:(void (^)(BOOL success, NSString *errorMessage))completion;
+- (void)logoutWithCompletion:(void(^)())completion;
+
+@property (nonatomic, readonly) QMAuthService* authService;
+@property (nonatomic, readonly) QMChatService* chatService;
+
+@end
