f8b947121c3710a08a9d3712a8e94ed21a665e7a
Andrey Moskvin <berkold90@gmail.com>
2015-10-23 15:59:59 +0300

    Mogenerator updated

--- Scripts/mogenerator.sh
+++ Scripts/mogenerator.sh
@@ -7,8 +7,6 @@ MACHINE_DIR="${CORE_DATA_DIRECTORY}/EntriesMachine"
 INCLUDE_H="${CORE_DATA_DIRECTORY}/$1"
 INPUT_FILE_PATH="${CORE_DATA_DIRECTORY}/$2"
 
-echo "${INPUT_FILE_PATH}/.xccurrentversion"
-
 curVer=`/usr/libexec/PlistBuddy "${INPUT_FILE_PATH}/.xccurrentversion" -c 'print _XCCurrentVersionName'`
 
 mogenerator=/usr/bin/mogenerator
@@ -25,4 +23,4 @@ echo "Begin mom for $3"
 echo $mogenerator --model \"${INPUT_FILE_PATH}\" --machine-dir "$MACHINE_DIR/" --human-dir "$HUMAN_DIR/" --includeh "$INCLUDE_H" --template-var arc=true
 $mogenerator --model \"${INPUT_FILE_PATH}\" --machine-dir "$MACHINE_DIR/" --human-dir "$HUMAN_DIR/" --includeh "$INCLUDE_H" --template-var arc=true
 
-echo "that's all folks. mogen.sh is done"
+echo "that's all folks. mogenerator.sh is done"
