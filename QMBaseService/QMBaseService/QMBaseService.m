d923035e0c07adc42ce939e346a9d53541c2e859
Ruslan Kavetsky <rusya.182@gmail.com>
2017-03-29 16:33:30 +0300

    Remove deferredQueueManager from base class, leave only in chat service

--- QMBaseService/QMBaseService/QMBaseService.m
+++ QMBaseService/QMBaseService/QMBaseService.m
@@ -11,11 +11,9 @@
 #import "QMSLog.h"
 
 
-@interface QMBaseService() <QMDeferredQueueManagerDelegate>
+@interface QMBaseService()
 
 @property (weak, nonatomic) id <QMServiceManagerProtocol> serviceManager;
-
-@property (strong, nonatomic, readwrite) QMDeferredQueueManager *deferredQueueManager;
 
 @end
 
@@ -33,26 +31,13 @@
 }
 
 - (void)serviceWillStart {
-    
-}
 
-- (QMDeferredQueueManager *)deferredQueueManager {
-    
-    static QMDeferredQueueManager *manager = nil;
-    static dispatch_once_t onceToken;
-    dispatch_once(&onceToken, ^{
-        
-        manager = [[QMDeferredQueueManager alloc] init];
-        [manager addDelegate:self];
-    });
-    
-    return manager;
 }
 
 #pragma mark - QMMemoryStorageProtocol
 
 - (void)free {
-    
+
 }
 
 @end
